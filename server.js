2ace76997a61703d659357930c1fd5048c8ef6ca
Sam <2726-sambergeland-WKCcKe@users.noreply.uci.bootcampcontent.com>
2022-06-21 07:58:50 -0700

    script fixed to properly deploy to heroku

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ app.use(express.static('public'))
 
 // "/notes" responds with the notes.html file
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../Develop/public/notes.html'));
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 // All other routes respond with the index.html file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../Develop/public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 
